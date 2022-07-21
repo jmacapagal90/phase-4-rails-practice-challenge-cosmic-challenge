944f9556776ecae20a225a24931cbbf950e68901
James Macapagal <j.macapagal@gmail.com>
2022-07-21 15:57:36 -0500

    finished CC, saving for ref

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :scientists
+  resources :missions, only: :create
+  resources :planets, only: :index
   # Defines the root path route ("/")
   # root "articles#index"
 end
